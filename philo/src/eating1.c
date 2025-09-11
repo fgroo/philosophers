a6cfa5b3fa1010af9aed8faf56a7a7d8ffe069a1
fgroo <faris.goerlich@gmail.com>
2025-09-11 20:27:53 +0200

    revert if statements, focousing on escaping thread

--- philo/src/eating1.c
+++ philo/src/eating1.c
@@ -6,7 +6,7 @@
 /*   By: fgroo <student@42.eu>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 06:17:49 by fgroo             #+#    #+#             */
-/*   Updated: 2025/09/03 21:30:46 by fgroo            ###   ########.fr       */
+/*   Updated: 2025/09/11 19:53:36 by fgroo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	eating(t_vars *vars, size_t philo_num)
 {
 	static pthread_mutex_t	print_mutex = PTHREAD_MUTEX_INITIALIZER;
 	const size_t			left = philo_num - 1;
-	const size_t			right = (vars->philo_num == 1) ? 1 : (philo_num % vars->philo_num);
+	const size_t			right = (philo_num % vars->philo_num);
 
 	pthread_mutex_lock(&print_mutex);
 	print_args(vars, 'e', philo_num);
@@ -26,18 +26,6 @@ void	eating(t_vars *vars, size_t philo_num)
 	pthread_mutex_lock(&vars->eaten_mutex);
 	++vars->eaten_count;
 	pthread_mutex_unlock(&vars->eaten_mutex);
-	if (philo_num % 2 == 1)
-	{
-		if (pthread_mutex_unlock(&vars->forks[right]) != 0)
-			vars->err = 1; /* Handle lock error, e.g., set err flag */
-		if (pthread_mutex_unlock(&vars->forks[left]) != 0)
-			vars->err = 1;
-	}
-	else
-	{
-		if (pthread_mutex_unlock(&vars->forks[left]) != 0)
-			vars->err = 1;
-		if (pthread_mutex_unlock(&vars->forks[right]) != 0)
-			vars->err = 1;
-	}
+	pthread_mutex_unlock(&vars->forks[left]);
+	pthread_mutex_unlock(&vars->forks[right]);
 }
--- philo/src/hub1.c
+++ philo/src/hub1.c
@@ -6,7 +6,7 @@
 /*   By: fgroo <student@42.eu>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 08:22:10 by fgroo             #+#    #+#             */
-/*   Updated: 2025/09/03 21:30:03 by fgroo            ###   ########.fr       */
+/*   Updated: 2025/09/11 20:19:41 by fgroo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,22 @@
 #include <stddef.h>
 #include <unistd.h>
 
-#include <stdlib.h> /* For malloc and free */
-
 static void	*inner_hub(t_vars *vars, size_t philo_num)
 {
 	const size_t	left = philo_num - 1;
-	const size_t	right = (vars->philo_num == 1) ? 1 : (philo_num % vars->philo_num);
+	const size_t	right = (philo_num % vars->philo_num);
 
 	while (!vars->err && !g_sigint && (vars->turns == 0 || vars->eaten_count < (vars->turns * vars->philo_num)))
 	{
-		/* Fix: Always lock both forks based on parity, without conditional on lock return */
-		if (philo_num % 2 == 1)
-		{
-			if (pthread_mutex_lock(&vars->forks[right]) != 0)
-				vars->err = 1; /* Handle lock error, e.g., set err flag */
-			if (pthread_mutex_lock(&vars->forks[left]) != 0)
-				vars->err = 1;
-		}
-		else
-		{
-			if (pthread_mutex_lock(&vars->forks[left]) != 0)
-				vars->err = 1;
-			if (pthread_mutex_lock(&vars->forks[right]) != 0)
-				vars->err = 1;
-		}
-		eating(vars, philo_num); /* Assumes this unlocks both forks in reverse order */
+		pthread_mutex_lock(&vars->forks[left]);
+		print_args(vars, 'f', philo_num); // (size_t){0} = write(1, "L\n", 3);
+		pthread_mutex_lock(&vars->forks[right]);
+		print_args(vars, 'f', philo_num); // , (size_t){0} = write(1, "R\n", 3);
+		eating(vars, philo_num);
 		sleeping(vars, philo_num);
 		thinking(vars, philo_num);
 	}
+	(size_t){0} = write(1, "somebody fled!!!\n", 17);
 	return (0);
 }
 
@@ -51,9 +39,9 @@ void	*portal(void *bypass)
 	t_bypass	*pass;
 
 	pass = (t_bypass *)bypass;
+
 	if (inner_hub(pass->vars, pass->philo_num))
-		return (free(pass), (void *)1);
-	free(pass);
+		return ((void *)1);
 	return (0);
 }
 
@@ -61,19 +49,14 @@ int	pre_hub(t_vars *vars)
 {
 	size_t		i;
 	size_t		j;
-	t_bypass	*arg;
 
 	i = 1;
 	j = 1;
 	while (i <= vars->philo_num)
 	{
-		arg = malloc(sizeof(t_bypass));
-		if (!arg)
+		if (pthread_create(&vars->philos[i], NULL, portal, &(t_bypass){vars, i}))
 			return (cleanup(vars, i), 1);
-		arg->vars = vars;
-		arg->philo_num = i;
-		if (pthread_create(&vars->philos[i], NULL, portal, arg))
-			return (free(arg), arg = NULL, cleanup(vars, i), 1);
+		usleep(100);
 		++i;
 	}
 	while (j <= vars->philo_num)
